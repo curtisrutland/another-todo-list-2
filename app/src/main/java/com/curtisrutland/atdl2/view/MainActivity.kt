1fbc7aa9a9ba79b9f684085126dd5115d11d48af
Curtis Rutland <curtisrutland@gmail.com>
2018-08-31 23:36:43 -0500

    added github icon and custom tab launch

--- app/src/main/java/com/curtisrutland/atdl2/view/MainActivity.kt
+++ app/src/main/java/com/curtisrutland/atdl2/view/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.curtisrutland.atdl2.view
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.curtisrutland.atdl2.R
 import com.curtisrutland.atdl2.adapter.TodoListCollectionAdapter
@@ -15,11 +20,10 @@ import com.curtisrutland.atdl2.extension.getDb
 import io.reactivex.android.schedulers.AndroidSchedulers
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.launch
-import org.jetbrains.anko.*
-import org.jetbrains.anko.coroutines.experimental.bg
 import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.coroutines.experimental.bg
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
@@ -31,6 +35,16 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         setContentView(R.layout.activity_main)
         setupRecyclerView()
         subscribeToTodoLists()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.projectLinkAction -> openProjectLink()
+        else -> super.onOptionsItemSelected(item)
     }
 
     private fun subscribeToTodoLists() {
@@ -82,7 +96,13 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         startActivity(intent)
     }
 
-    private fun deleteTodoList(todoList: TodoList): Deferred<Unit> = bg {
-        getDb().deleteTodoList(todoList)
+    private fun openProjectLink(): Boolean {
+        val projectLink = getString(R.string.project_link)
+        val uri = Uri.parse(projectLink)
+        CustomTabsIntent.Builder()
+                .setToolbarColor(ContextCompat.getColor(this, R.color.colorBlack))
+                .build()
+                .launchUrl(this, uri)
+        return true
     }
 }
